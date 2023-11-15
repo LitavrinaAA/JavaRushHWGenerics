224d03e4b638fc7a7dbc354e267031699f4da6d8
LitavrinaA <a.a.litavrina@gmail.com>
2023-11-15 22:30:48 +0300

    first

--- src/main/java/org/example/MyList.java
+++ src/main/java/org/example/MyList.java
@@ -1,29 +1,134 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Objects;
 import java.util.function.Function;
 
-public class MyList {
-  public void add(Object o) {
-    throw new RuntimeException("Not implemented");
+/** Описать разницу между статическим InnerClass и обычным InnerClass
+ *  Вложенные классы делятся на две категории: статические и нестатические.
+ *  Вложенные классы, которые объявлены статическими, называются просто
+ *  статическими вложенными классами.
+ *  Нестатические вложенные классы называются внутренними классами.
+ *
+ *  Доступ к статическим вложенным классам осуществляется с использованием имени внешнего класса
+ *  OuterClass.StaticNestedClass
+ *
+ *  Чтобы создать экземпляр внутреннего класса, необходимо сначала создать экземпляр внешнего класса.
+ *  Затем создайте внутренний объект внутри внешнего объекта
+ *  class OuterClass {
+ *     ...
+ *     class InnerClass {
+ *         ...
+ *     }
+ *  }
+ * OuterClass outerObject = new OuterClass()
+ * OuterClass.InnerClass innerObject = outerObject.new InnerClass();
+ *
+ */
+
+public class MyList<T extends Number> implements Iterable<T> {
+  protected int modCount = 0;
+ T[] elementData;
+  private int size;
+  private static final int DEFAULT_CAPACITY = 10;
+    private final int CUT_RATE = 4;
+
+
+  public MyList(){
+      this.elementData = (T[]) new Number[DEFAULT_CAPACITY];
+  }
+  public T add(T t) {
+    if(size == elementData.length-1)
+      resize(elementData.length*2); // увеличу в 2 раза, если достигли границ
+    elementData[size++] = t;
+    return t;
+
   }
 
-  public Object get(int index) {
-    throw new RuntimeException("Not implemented");
+  public T get(int index) {
+      return (T) elementData[index];
   }
 
-  private void resize() {
-    throw new RuntimeException("Not implemented");
+  private void resize(int newSize) {
+    T[] newArray = (T[]) new Number[newSize];
+    System.arraycopy(elementData, 0, newArray, 0, size);
+    elementData = newArray;
   }
 
-  public Object remove(int index) {
-    throw new RuntimeException("Not implemented");
+  public T remove(int index) {
+      T removeElement = (T) elementData[index];
+      for (int i = index; i< size; i++)
+          elementData[i] = elementData[i+1];
+      elementData[size] = null;
+      size--;
+      if (elementData.length > DEFAULT_CAPACITY && size < elementData.length / CUT_RATE)
+          resize(elementData.length/2);
+      // если элементов в CUT_RATE раз меньше чем
+      // длина массива, то уменьшу в два раза
+      return removeElement;
   }
 
-  public MyList map(Function f) {
-    throw new RuntimeException("Not implemented");
+  public <F extends Number> MyList map(Function<T, F> f) {
+    //  list.stream().mapToInt(f).count();
+      MyList<F> mappedList = new MyList<>();
+      for (int i = 0; i < size; i++) {
+          mappedList.add(f.apply((T) elementData[i]));
+      }
+
+      return mappedList;
   }
 
   public int size() {
-    throw new RuntimeException("Not implemented");
+    return size;
   }
+
+    @Override
+    public String toString() {
+        return "MyList{" +
+                "elementData=" + Arrays.toString(elementData) +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MyList<?> myList = (MyList<?>) o;
+        return size == myList.size && Arrays.equals(elementData, myList.elementData);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(size);
+        result = 31 * result + Arrays.hashCode(elementData);
+        return result;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new MyIterator();
+    }
+    public class MyIterator implements Iterator<T> {
+        int index = 0;
+
+        public MyIterator() {
+            this.index = 0;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return index < size();
+        }
+
+        @Override
+        public T next() {
+            return elementData[index++];
+        }
+
+        @Override
+        public void remove() {
+           MyList.this.remove(index);
+        }
+    }
 }
--- src/test/java/org/example/MyListTest.java
+++ src/test/java/org/example/MyListTest.java
@@ -3,9 +3,174 @@ package org.example;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Iterator;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
 public class MyListTest {
-  @Test
-  public void test() {
-    Assertions.assertTrue(true);
-  }
+    private MyList<Integer> myList = new MyList<>();
+
+    @Test
+    void add() {
+        myList = new MyList<>();
+        Integer res = myList.add(1);
+
+        assertEquals("1", res);
+    }
+
+    @Test
+    void get() {
+        myList = new MyList<>();
+        myList.add(1);
+        myList.add(2);
+        myList.add(3);
+        myList.add(4);
+        myList.add(5);
+        Integer res = myList.get(0);
+
+        assertEquals(1, res);
+
+    }
+
+    @Test
+    void remove() {
+        myList = new MyList<>();
+        myList.add(1);
+        myList.add(2);
+        myList.remove(0);
+        int res = myList.size();
+        assertEquals(1, res);
+    }
+
+    @Test
+    void map() {
+        myList = new MyList<>();
+        myList.add(1);
+        myList.add(2);
+        myList.add(3);
+        myList.add(4);
+        myList.add(5);
+        MyList<Integer> myListNew = myList.map(x -> x + 1);
+        Integer res = myList.get(2);
+        assertEquals(4, res);
+    }
+
+    @Test
+    void equals() {
+        MyList<Integer> myList1 = new MyList<>();
+        myList1.add(1);
+        myList1.add(2);
+        MyList<Integer> myList2 = new MyList<>();
+        myList2.add(3);
+        myList2.add(4);
+        MyList<Integer> myList3 = myList1;
+        Assertions.assertTrue(myList1.equals(myList2), "в листах 1 и 2 разные значения");
+        Assertions.assertFalse(myList3.equals(myList1), "Листы скопированны, одинаковые");
+        Assertions.assertTrue(myList2.equals(myList3), "в листах 2 и 3 разные значения");
+
+
+    }
+
+    @Test
+    void testHashCode() {
+        MyList<Integer> myList1 = new MyList<>();
+        myList1.add(1);
+        myList1.add(2);
+        MyList<Integer> myList2 = new MyList<>();
+        myList2.add(3);
+        myList2.add(4);
+        MyList<Integer> myList3 = myList1;
+        MyList<Integer> myList4 = new MyList<>();
+        myList4.add(1);
+        myList4.add(2);
+        int res1 = myList1.hashCode();
+        int res2 = myList2.hashCode();
+        int res3 = myList3.hashCode();
+        int res4 = myList4.hashCode();
+        Assertions.assertNotEquals(res1, res2);
+        Assertions.assertEquals(res1, res3);
+        Assertions.assertEquals(res1, res4, "4-ый лист заполнен теми же значениями что и 1-ый");
+    }
+//  test hasNext on an empty collection (returns false)
+//  test next() on an empty collection (throws exception)
+//  test hasNext on a collection with one item (returns true, several times)
+//  test hasNext/next on a collection with one item: hasNext returns true, next returns the item, hasNext returns false, twice
+//  test remove on that collection: check size is 0 after
+//  test remove again: exception
+//  final test with a collection with several items, make sure the iterator goes through each item, in the correct order (if there is one)
+//  remove all elements from the collection: collection is now empty
+
+
+    @Test
+    void hasNext() {
+        myList = new MyList<>();
+        Assertions.assertFalse(myList.iterator().hasNext());
+        myList.add(1);
+        myList.add(2);
+        Assertions.assertTrue(myList.iterator().hasNext());
+    }
+
+    @Test
+    void next() {
+        myList = new MyList<>();
+        Iterator<Integer> iter1 = myList.iterator();
+        Integer res = iter1.next();
+        Assertions.assertEquals(null, res);
+
+        myList.add(1);
+        myList.add(2);
+        Iterator<Integer> iter2 = myList.iterator();
+        res = iter2.next();
+        res = iter2.next();
+        Assertions.assertEquals(2, res);
+    }
+
+    @Test
+    void testErrAdd() {
+        myList = new MyList<>();
+
+        Iterator<Integer> iter = myList.iterator();
+        iter.next();
+        iter.remove();
+
+        ArrayIndexOutOfBoundsException thromn = assertThrows(
+                ArrayIndexOutOfBoundsException.class,
+                () -> myList.add(1), "Expected add() to throw, but it didn't"
+        );
+
+
+    }
+
+    @Test
+    void testRemove() {
+        myList = new MyList<>();
+        myList.add(1);
+        Iterator<Integer> iter = myList.iterator();
+        iter.next();
+        iter.remove();
+        int res = myList.size();
+        Assertions.assertEquals(0, res);
+
+    }
+
+    @Test
+    void iterAll() {
+        myList = new MyList<>();
+
+        myList.add(1);
+        myList.add(2);
+        myList.add(3);
+        myList.add(4);
+        myList.add(5);
+
+        Iterator<Integer> iter = myList.iterator();
+        int index = 0;
+        while (iter.hasNext()) {
+            Integer res = iter.next();
+            Assertions.assertEquals(myList.get(index++), res);
+        }
+
+    }
 }
